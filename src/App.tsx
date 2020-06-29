d56d5af07a889032c75daa3dc8113b799d7a0b1a
timardex <timardex@gmail.com>
2020-06-29 09:38:32 +0200

    adding overflow to html

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/templates/Header';
 import Authentication from './components/templates/Authentication';
 
 const App: React.FC<{}> = () => {
   const [toggleAuth, setToggleAuth] = useState<boolean>(false);
+
+  useEffect(() => {
+    const html = document.getElementsByTagName('html')[0];
+    toggleAuth ? (html.style.overflowY = 'hidden') : (html.style.overflowY = 'auto');
+  })
+
   return (
     <div className="App">
       <Header showAuth={setToggleAuth} />
